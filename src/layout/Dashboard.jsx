600bc3d04e36fdec37e19ab61969af5f0a147120
Adınız <erkanmurat0632@gmail.com>
2024-06-01 23:35:37 +0300

    camp project started

---
+++ src/layout/Dashboard.jsx
@@ -0,0 +1,16 @@
+// rfc
+
+import React from 'react'
+import Navy from './Navy'
+import Categories from './Categories'
+import ProductList from '../pages/ProductList'
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Navy />
+      <Categories />
+      <ProductList />
+    </div>
+  )
+}
